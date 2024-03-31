b62ebb91aa97c18cdde98452b9adcb583f654814
maciejwiacek <wiacem811_aehit@students.vizja.pl>
2024-03-31 10:59:25 +0200

    First version of hourly forecast and beginning of designing daily forecast

---
+++ WeatherApp/HourlyForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  HourlyForecastView.swift
+//  WeatherApp
+//
+//  Created by Maciej Wiącek on 30/03/2024.
+//
+
+import SwiftUI
+
+struct HourlyForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HourlyForecastView()
+}
